54f450c9b50f3caee061b5afdedf8360fc84ff9b
Joshua <joshuabongz7@gmail.com>
2025-10-05 22:07:15 +0800

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import EarthquakeEventsList from "./components/EarthquakeEventsList";
 import "./App.css";
 import "./index.css";
-import EarthquakeEventsList from "./components/EarthquakeEventsList";
 
 function App() {
   return (
